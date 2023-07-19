47a71449fe7ce28085d9b162a0f7a20525517419
DamirDyanov <damirdyanov@yandex.ru>
2023-07-19 21:12:21 +0300

    enter to issues

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-print("Hello")
-print(5*6)
-print(5)
-print(6)
+from playwright.sync_api import sync_playwright, expect
+import pytest
+import click
+
+def test1():
+    with sync_playwright() as s:
+        for browser_type in [s.chromium]:
+            browser = browser_type.launch()
+            page = browser.new_page()
+            #locator = "//span[text()='123']"
+            page.goto('https://github.com/DamirDyanov?tab=repositories')
+            page.get_by_role("link", name="123").click()
+            page.get_by_role("link", name='Issues').click()
+            page.locator("//a[contains(text(),'Ошибка в первой строке')]").click()
+            page.locator("//div[@class='timeline-comment-header clearfix d-flex']").click()
+            page.screenshot(path=f'example-{browser_type.name}.png')
+            browser.close()
+
+
